305e4997248893a380f2fbbc4fb1f6595986e067
Yabiti <yabetseyohannes@gmail.com>
2024-08-29 09:43:48 +0300

    .

--- Marketplace/item/models.py
+++ Marketplace/item/models.py
@@ -16,3 +16,4 @@ class Item(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     price = models.FloatField()
+    is_sold = models.BooleanField(default=False)
