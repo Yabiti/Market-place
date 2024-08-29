5bd52170aaf3d5f5016d67c9c321ef9ee45d8a9e
Yabiti <yabetseyohannes@gmail.com>
2024-08-29 10:43:32 +0300

    .

--- Marketplace/sell/views.py
+++ Marketplace/sell/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 
+from . import Category, Item
 # Create your views here.
 def index(request):
+    items = Item.objects.filter(is_sild=False)[0:6]
     return render(request, 'sell/index.html')
 
 def contact(request):
