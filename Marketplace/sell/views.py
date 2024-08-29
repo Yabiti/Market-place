597443c25b4eb07061dee00fb4eb2d26246d0e32
Yabiti <yabetseyohannes@gmail.com>
2024-08-29 10:45:35 +0300

    .

--- Marketplace/sell/views.py
+++ Marketplace/sell/views.py
@@ -4,7 +4,10 @@ from . import Category, Item
 # Create your views here.
 def index(request):
     items = Item.objects.filter(is_sild=False)[0:6]
-    return render(request, 'sell/index.html')
+    Categories = Category.objects.all()
+    return render(request, 'sell/index.html'{
+        'Categories': Categories
+    })
 
 def contact(request):
     return render(request, 'sell/contact.html')
