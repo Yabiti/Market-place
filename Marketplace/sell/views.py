41e649f52fe3b5f5032afc24ef2669803a0bd996
Yabiti <yabetseyohannes@gmail.com>
2024-09-03 12:30:46 +0300

    .

--- Marketplace/sell/views.py
+++ Marketplace/sell/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 
-from . import Category, Item
+from item.models import Category, Item
 # Create your views here.
 def index(request):
-    items = Item.objects.filter(is_sild=False)[0:6]
+    items = Item.objects.filter(is_sold=False)[0:6]
     Categories = Category.objects.all()
     return render(request, 'sell/index.html',{
         'Categories': Categories,
