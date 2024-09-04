f5409b82b11431fc015916de5da0afaa8b3b95aa
Yabiti <yabetseyohannes@gmail.com>
2024-09-04 14:01:48 +0300

    .

--- Marketplace/Marketplace/urls.py
+++ Marketplace/Marketplace/urls.py
@@ -25,4 +25,4 @@ urlpatterns = [
     path('contact', contact, name='contact'),
     path('base', base, name='base'),
     path('admin/', admin.site.urls),
-]
+] * static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
