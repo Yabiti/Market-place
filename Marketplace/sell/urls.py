160de9a5af66ae52fe39055c84e770af129b5484
Yabiti <yabetseyohannes@gmail.com>
2024-09-06 12:41:34 +0300

    .

--- Marketplace/sell/urls.py
+++ Marketplace/sell/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = 'sell'
 
 urlpatterns = [
-    path('', views.index, name='index')    
+    path('', views.index, name='index'),
+    path('contact', views.contact, name='contact'),
+ 
 ]
