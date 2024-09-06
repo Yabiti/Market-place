a9d9012df5d798f75b213d44d7dcb993fbfaa1f2
Yabiti <yabetseyohannes@gmail.com>
2024-09-06 12:43:58 +0300

    .

--- Marketplace/Marketplace/urls.py
+++ Marketplace/Marketplace/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from sell.views import index, contact, base
 
 urlpatterns = [
     path('', include('sell.urls')),
