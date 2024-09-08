eaafbbc942cb45c0f91064fad27bca939ddc6ad1
Yabiti <yabetseyohannes@gmail.com>
2024-09-08 09:41:57 +0300

    .

--- Marketplace/sell/forms.py
+++ Marketplace/sell/forms.py
@@ -3,3 +3,6 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class SignUpForm(UserCreationForm):
+    class Meta:
+        model = User
+        
