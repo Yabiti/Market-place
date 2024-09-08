413c2a703304e20fe3b7071b42a76c0f319ac3f7
Yabiti <yabetseyohannes@gmail.com>
2024-09-08 11:41:58 +0300

    .

--- Marketplace/sell/forms.py
+++ Marketplace/sell/forms.py
@@ -9,4 +9,21 @@ class SignUpForm(UserCreationForm):
 
     username = forms.CharField(widget=forms.TextInput(attrs={
         'placeholder': "Your UserName",
+        'class': 'w-full py-4 px-6 rounded-xl'
+    }))
+
+    email = forms.CharField(widget=forms.EmailInput(attrs={
+        'placeholder': "Your email",
+        'class': 'w-full py-4 px-6 rounded-xl'
+    }))
+
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': "Your password",
+        'class': 'w-full py-4 px-6 rounded-xl'
+    }))
+
+
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': "Your password",
+        'class': 'w-full py-4 px-6 rounded-xl'
     }))
