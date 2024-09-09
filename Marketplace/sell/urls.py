a382d3d6870010cb91fccc73c53dcc38b64233f6
Yabiti <yabetseyohannes@gmail.com>
2024-09-09 09:11:54 +0300

    .

--- Marketplace/sell/urls.py
+++ Marketplace/sell/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('contact', views.contact, name='contact'),
     path('signup/', views.signup, name='signup'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
  
 ]
