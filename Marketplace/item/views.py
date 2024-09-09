883efdc445aada80c948aee9c4d346f211610ecc
Yabiti <yabetseyohannes@gmail.com>
2024-09-09 10:26:16 +0300

    .

--- Marketplace/item/views.py
+++ Marketplace/item/views.py
@@ -16,6 +16,6 @@ def detail(request, pk):
 def new(request):
     form = NewItemForm()
 
-    return render(request, 'item/login.html',{
+    return render(request, 'item/form.html',{
         'form': form,
 })
