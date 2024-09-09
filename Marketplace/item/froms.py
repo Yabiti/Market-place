5760e0fc33feef1d68160f3faf466572b39d8437
Yabiti <yabetseyohannes@gmail.com>
2024-09-09 09:35:40 +0300

    .

--- Marketplace/item/froms.py
+++ Marketplace/item/froms.py
@@ -5,4 +5,4 @@ from .models import item
 class NewItemForm(forms.ModelForm):
     class Meta:
         model = item
-        fields = ('category', 'name', 'price', )
+        fields = ('category', 'name', 'price', 'image')s
