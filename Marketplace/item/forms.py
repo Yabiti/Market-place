b1dc4dea3c392844c3c2c51298bf5f209060e930
Yabiti <yabetseyohannes@gmail.com>
2024-12-03 04:04:03 -0800

    Force remove all files

--- Marketplace/item/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-from .models import Item
-
-class NewItemForm(forms.ModelForm):
-    class Meta:
-        model = Item
-        fields = ('Category', 'name', 'price', 'image')
