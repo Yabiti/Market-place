a621c49b9628a2c890bbc31ebd1a698976ce22eb
Yabiti <yabetseyohannes@gmail.com>
2024-12-12 04:15:50 -0800

    .

--- sell/core/views.py
+++ sell/core/views.py
@@ -14,10 +14,22 @@ def index(request):
 def contact(request):
     return render(request, "core/contact.html")
 
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from .forms import SignUpForm
 
 def signup(request):
-    form = SignUpForm()
-
-    return render(request, 'core/signup.html', {
-        'from': form
-    })
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.refresh_from_db()  # Load the profile instance created by the signal
+            user.email = form.cleaned_data.get('email')
+            user.save()
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=user.username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
