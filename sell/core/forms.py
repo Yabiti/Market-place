79232f37188dd67089a2c31cd36328655c7b694f
Yabiti <yabetseyohannes@gmail.com>
2024-12-12 03:58:18 -0800

    .

--- sell/core/forms.py
+++ sell/core/forms.py
@@ -1,3 +1,7 @@
 from django import froms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+
+class SignUpForm(UserCreationForm):
+    class Meta:
+        model = User
