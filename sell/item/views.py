fe067b5b17ada2c7e3d468384615d81e311c7310
Yabiti <yabetseyohannes@gmail.com>
2024-12-12 03:46:21 -0800

    .

--- sell/item/views.py
+++ sell/item/views.py
@@ -5,7 +5,7 @@ from .models import Item
 
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
-    related_items = item.objects.filter(category= item.category, is_sold=False).exclude(pk=pk)[0:3]
+    related_items = Item.objects.filter(category= item.category, is_sold=False).exclude(pk=pk)[0:3]
 
 
     return render(request, 'item/detail.html', {
