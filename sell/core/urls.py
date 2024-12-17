bed2554854a98aeaafdd380de0af96bf6b4b511a
Yabiti <yabetseyohannes@gmail.com>
2024-12-17 00:15:57 -0800

    ..

--- sell/core/urls.py
+++ sell/core/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
+
 
 from . import views
 
@@ -8,5 +10,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path('contact/', views.contact, name="contact"),
     path('signup/', views.signup, name="signup"),
+    path('login/', auth_views.LoginView.as_view)
 
 ]
