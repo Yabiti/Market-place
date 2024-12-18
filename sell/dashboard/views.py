085d7f8e4b16eb6495f9d44e606290508bf37f90
Yabiti <yabetseyohannes@gmail.com>
2024-12-18 00:34:01 -0800

    .

--- sell/dashboard/views.py
+++ sell/dashboard/views.py
@@ -7,3 +7,7 @@ from item.models import Item
 @login_required
 def index(request):
     items = Item.objects.filter(created_by=request.user)
+
+    return render(request, 'dashboard/index.html', {
+        'items': items
+    })
