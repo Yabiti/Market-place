fe62ecfbe99b6e68446f065a6b48256791b4f9bc
Yabiti <yabetseyohannes@gmail.com>
2024-12-19 02:57:45 -0800

    .

--- sell/sell/urls.py
+++ sell/sell/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('item/', include(('item.urls'))),
     path('dashboard/', include(('dashboard.urls'))),
+    path('inbox/', include(('conversation.urls')))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
  
