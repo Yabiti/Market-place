c4cfb8c6d9afac05308b01fc3b14e99de36d33e4
Yabiti <yabetseyohannes@gmail.com>
2025-01-01 23:42:17 -0800

    .

--- sell/core/views.py
+++ sell/core/views.py
@@ -9,7 +9,7 @@ def index(request):
     items = Item.objects.filter(is_sold=False)[0:6]
     categories = Category.objects.all()
     return render(request, "core/index.html", {
-        'catego ries': categories,
+        'categories': categories,
         'items': items
     })
 
